fa98252bd2b34d88cd7254f2d0d36d98e474ca20
jameslindfors <jameslindfors@outlook.com>
2021-12-16 13:32:29 -0800

    Tailwind Styling and Canvas Init

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,25 @@
 <script>
+    import {onMount} from 'svelte';
     import init, { add } from 'algos-ds-wasm';
 
+    let canvas;
+
+    // WASM Magic
 const maths = async () => {
   await init();
 
   console.log('Addition in rust:', add(1, 2));
 };
 
-maths();
+    onMount(() => {
+        maths();
+
+        const ctx = canvas.getContext('2d');
+        ctx.beginPath();
+        ctx.arc(145, 75, 40, 0, 2 * Math.PI);
+        ctx.stroke();
+
+    });
 </script>
 <svelte:head>
     <title>Home</title>
@@ -15,9 +27,15 @@ maths();
 
 <div class="app">
     <header>
-        <h1 class="text-blue-600 text-5xl">
-            Snowpack Svelte
-        </h1>
+        <h2 class="font-bold text-3xl mx-4 py-4">Sorting Algorithms</h2>   
     </header>
+    <div class="controls container mx-4 px-1 flex flex-col w-44">
+        <button class="genNewArr bg-transparent hover:bg-blue-700 text-blue-500 font-semibold hover:text-white py-1.5 px-4 my-1 border border-blue-500 hover:border-transparent rounded">New Array</button>
+        <button class="mergeSort bg-transparent hover:bg-blue-700 text-blue-500 font-semibold hover:text-white py-1.5 px-4 my-1 border border-blue-500 hover:border-transparent rounded">Merge Sort</button>
+        <label for="speed" class="text-blue-500 font-bold">Set Animation Speed:</label>
+        <input type="range" name="speed" id="speed" min="0" max="10" value="0" class="py-2 ">
+    </div>
+    <canvas style="border: 1px solid black;" bind:this={canvas}>
+    </canvas>
 
 </div>
